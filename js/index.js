4f4456cffa88fa4534d71216fef151bc0e8d3b46
mohammed <simotenerife@hotmail.com>
2017-03-18 15:17:33 +0000

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,5 @@
+var PSV = PhotoSphereViewer({
+  panorama:  'https://raw.githubusercontent.com/momah/foto/master/fisica.jpg',//'URL_DE_LA_IMAGEN'
+  container: 'photosphere',//ID_SIN_#_DEL_DIV_DONDE_IRA_LA_IMAGEN
+  loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif',
+});
